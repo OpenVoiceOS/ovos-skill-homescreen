aa252dd214d208fb4a376ed13bb7a7481b604e22
builderjer <builderjer@gmail.com>
2025-10-26 10:18:26 -0600

    update pypi name update requrements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import walk, path
 
 URL = "https://github.com/OpenVoiceOS/ovos-skill-homescreen"
 SKILL_CLAZZ = "OVOSHomescreenSkill"  # needs to match __init__.py class name
-PYPI_NAME = "ovos-skill-homescreen"  # pip install PYPI_NAME
+PYPI_NAME = "ovos_skill_homescreen"  # pip install PYPI_NAME
 
 
 # below derived from github url to ensure standard skill_id
